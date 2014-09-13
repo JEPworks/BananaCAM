4cc4a3929aae66ceb2ae4354aa52bcc58aa9e4d9
Xavier A. Loza Morales <xavier.morales@veeva.com>
2014-09-13 04:04:05 -0700

    Fixed splash timing

--- app/src/main/java/com/ripeyet/bananacam/SplashScreenActivity.java
+++ app/src/main/java/com/ripeyet/bananacam/SplashScreenActivity.java
@@ -14,7 +14,7 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
  * Created by Xavier Morales on 9/12/2014.
  */
 public class SplashScreenActivity extends Activity {
-    private static final int SPLASH_DISPLAY_LENGTH = 3000;
+    private static final int SPLASH_DISPLAY_LENGTH = 2123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
