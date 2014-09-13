ddd65183141b89ecb300f9eee2dac64ef416cc8f
James Palmer <jamesepalmer@live.com>
2014-09-13 03:51:56 -0700

    Added Swipe to Refresh Widget

--- app/src/main/java/com/ripeyet/bananacam/tasks/BananaViewerFragment.java
+++ app/src/main/java/com/ripeyet/bananacam/tasks/BananaViewerFragment.java
@@ -3,8 +3,10 @@ package com.ripeyet.bananacam.tasks;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +33,53 @@ public class BananaViewerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_room_selection, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_room_selection, container, false);
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipelayout);
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE,Color.RED,Color.GREEN,Color.YELLOW);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                final ImageView imageView = (ImageView) rootView.findViewById(R.id.ivCurrentBanana);
+                final ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.pbImages);
+                OnBananaReturnHandler handler = new OnBananaReturnHandler() {
+                    @Override
+                    public void onCallComplete(String url) {
+                        ImageLoader imageLoader = ImageLoader.getInstance();
+                        imageLoader.displayImage(url, imageView, new ImageLoadingListener() {
+                            @Override
+                            public void onLoadingStarted(String imageUri, View view) {
+
+                            }
+
+                            @Override
+                            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                                progressBar.setVisibility(View.GONE);
+                                ((ImageView) view).setImageResource(R.drawable.distressed_banana);
+                                swipeRefreshLayout.setRefreshing(false);
+                            }
+
+                            @Override
+                            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                                progressBar.setVisibility(View.GONE);
+                                swipeRefreshLayout.setRefreshing(false);
+                            }
+
+                            @Override
+                            public void onLoadingCancelled(String imageUri, View view) {
+                                progressBar.setVisibility(View.GONE);
+                                swipeRefreshLayout.setRefreshing(false);
+                            }
+                        });
+
+                    }
+                };
+
+                LoadImageTask task = new LoadImageTask(getActivity().getBaseContext(),
+                        position, handler);
+                task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+            }
+        });
         return rootView;
     }
 
