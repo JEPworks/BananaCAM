019b489003170893ef22ab9dac8e71731a36b388
Xavier A. Loza Morales <xavier.morales@veeva.com>
2014-09-12 22:46:45 -0700

    Added Splash Screen

---
+++ app/src/main/java/com/ripeyet/bananacam/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.ripeyet.bananacam;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+
+/**
+ * Created by Xavier Morales on 9/12/2014.
+ */
+public class SplashScreenActivity extends Activity{
+    private static final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashpage);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this, RoomSelectionActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
